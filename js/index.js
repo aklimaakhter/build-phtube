bceb28b444e84df2c452e75d7efb5670d2d9cc52
Aklima Akhter <aklima00191@gmail.com>
2023-09-02 23:24:47 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const handleCategories = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data = await res.json();
+    
 
     const categoryContainer = document.getElementById('category-container');
 
-    // console.log(data.data)
+    console.log(data.data)
     data.data.forEach(category => {
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
@@ -20,6 +21,7 @@ const handleLoadCategory = async (categoryID) => {
     const data = await res.json()
     console.log(data.data)
     const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
     data.data.forEach(categories => {
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML =`
@@ -27,7 +29,7 @@ const handleLoadCategory = async (categoryID) => {
                 <figure class="px-10 pt-10">
                     <img src="${categories.thumbnail}"class ="h-[150px]" alt="Shoes" class="rounded-xl" />
                 </figure>
-                <div class="card-body items-center text-center ">
+                <div class="card-body">
                     <div class ="flex justify-center  space-y-4">
                     <div class ="mr-4">
                         <img src="${categories?.authors[0]?.profile_picture}" class="h-[50px] w-[50px] rounded-full" alt=""/>
@@ -36,7 +38,7 @@ const handleLoadCategory = async (categoryID) => {
                         <h1>${categories.title}</h1>
                         <div class="flex justify-center items-center">
                             <h5>${categories?.authors[0]?.profile_name}</h5>
-                            <p></p>
+                            <p>${categories.authors[0].verified ? "" : ''}</p>
                         </div>
                         <p>${categories.others.views} <span>viwes</span></p>
                     </div>
@@ -49,3 +51,11 @@ const handleLoadCategory = async (categoryID) => {
     })
 }
 handleCategories()
+handleLoadCategory('1000')
+
+
+// body{
+//     ${item.authors[0].verified ? "<img src='./images/download.png' class=' w-5 h-5' >" : ""}
+
+
+// "https://img.icons8.com/?size=2x&id=98A4yZTt9abw&format=png"
